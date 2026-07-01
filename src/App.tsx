d735dd441d7e4603d8c04412e6c1536f166fa1f2
tuanvik206 <tuanvik206@users.noreply.github.com>
2026-07-02 04:37:14 +0700

    Fix admin route handling for static hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Portfolio from './components/Portfolio';
 import Admin from './components/Admin';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Portfolio />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
